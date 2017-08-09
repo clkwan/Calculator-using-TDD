7dcb943667e238fe223fb77272c03d73d18aa963
clkwan <clkwan93@gmail.com>
2017-08-08 22:51:41 -0400

    not finished still need to get logging done

--- calculator.py
+++ calculator.py
@@ -1,10 +1,12 @@
+num = []
+
 
 def add(num):
-    for i in num:
-        if i == str:
-            print('Please enter a valid number')
-        else:
-            return sum(num)
+    try:
+        return sum(num)
+    except TypeError:
+        pass
+
 
 # def subtract():
 #
@@ -29,23 +31,31 @@ def add(num):
 #
 # def range():
 
-if __name__ == '__main__':
+def get_num():
     print('Welcome to William\'s Calculator!')
-    operation = input('Please choose from the following operations:\n'
-                      '1. addition\n'
-                      '2. subtraction\n'
-                      '3. multiplication\n'
-                      '4. division\n'
-                      '5. square root\n'
-                      '6. mean\n'
-                      '7. power\n'
-                      '8. cosine\n'
-                      '9. range\n')
-    if operation == 1:
-        num = []
-        try:
-            num = input('Please input the numbers you would like to sum up. Inputs should be separated by a space')
-            num = num.split(' ')
-        except ValueError:
-            print('That was not a valid number please try again.')
-        add(num)
+    operation = int(input('Please choose from the following operations:\n'
+                          '1. addition\n'
+                          '2. subtraction\n'
+                          '3. multiplication\n'
+                          '4. division\n'
+                          '5. square root\n'
+                          '6. mean\n'
+                          '7. power\n'
+                          '8. cosine\n'
+                          '9. range\n'))
+    try:
+        if operation in (1, 2, 3, 4, 6, 7, 9):
+            num.append(int(input('Please enter your first number: ')))
+            num.append(int(input('Please enter your second number: ')))
+            return num
+        elif operation in (5, 8):
+            num.append(int(input('Please enter your first number: ')))
+            return num
+        else:
+            print('Number is not an operation')
+    except ValueError:
+        print('Please enter a valid number')
+
+
+if __name__ == '__main__':
+    print(add(get_num()))
