3fb595be8eeeec6293d0f925ccb67ab44f5ef16d
clkwan <clkwan93@gmail.com>
2017-08-09 20:03:36 -0400

    still need to do unit tests

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-num = []
+import math
 
 
 def add(num):
@@ -8,30 +8,58 @@ def add(num):
         pass
 
 
-# def subtract():
-#
-#
-# def multiply():
-#
-#
-# def divide():
-#
-#
-# def square_root():
-#
-#
-# def mean():
-#
-#
-# def power():
-#
-#
-# def cosine():
-#
-#
-# def range():
+def subtract(num):
+    try:
+        return num[0]-num[1]
+    except TypeError:
+        pass
 
-def get_num():
+
+def multiply(num):
+    try:
+        return num[0]*num[1]
+    except TypeError:
+        pass
+
+
+def divide(num):
+    try:
+        return num[0]/num[1]
+    except TypeError:
+        pass
+    except ZeroDivisionError:
+        pass
+
+
+def square_root(num):
+    try:
+        return math.sqrt(num[0])
+    except TypeError:
+        pass
+
+
+def mean(num):
+    try:
+        return float(sum(num)) / max(len(num), 1)
+    except TypeError:
+        pass
+
+
+def power(num):
+    try:
+        return num[0]**num[1]
+    except TypeError:
+        pass
+
+
+def cosine(num):
+    try:
+        return math.cos(num[0])
+    except TypeError:
+        pass
+
+
+def get_operation():
     print('Welcome to William\'s Calculator!')
     operation = int(input('Please choose from the following operations:\n'
                           '1. addition\n'
@@ -43,10 +71,19 @@ def get_num():
                           '7. power\n'
                           '8. cosine\n'
                           '9. range\n'))
+    return operation
+
+
+def get_num(operation):
     try:
-        if operation in (1, 2, 3, 4, 6, 7, 9):
+        if operation in (2, 3, 4, 7, 9):
             num.append(int(input('Please enter your first number: ')))
             num.append(int(input('Please enter your second number: ')))
+            return num
+        elif operation in (1, 6):
+            count = int(input('Please enter the amount of numbers you would like to enter: '))
+            for i in range(count):
+                num.append(int(input('Please enter your number: ')))
             return num
         elif operation in (5, 8):
             num.append(int(input('Please enter your first number: ')))
@@ -58,4 +95,61 @@ def get_num():
 
 
 if __name__ == '__main__':
-    print(add(get_num()))
+        num = []
+        operation = get_operation()
+        if operation == 1:
+            answer = add(get_num(operation))
+            if answer is None:
+                pass
+            else:
+                print('Your answer is {}'.format(answer))
+        elif operation == 2:
+            answer = subtract(get_num(operation))
+            if answer is None:
+                pass
+            else:
+                print('Your answer is {}'.format(answer))
+        elif operation == 3:
+            answer = multiply(get_num(operation))
+            if answer is None:
+                pass
+            else:
+                print('Your answer is {}'.format(answer))
+        elif operation == 4:
+            answer = divide(get_num(operation))
+            if answer is None:
+                pass
+            else:
+                print('Your answer is {}'.format(answer))
+        elif operation == 5:
+            answer = square_root(get_num(operation))
+            if answer is None:
+                pass
+            else:
+                print('Your answer is {}'.format(answer))
+        elif operation == 6:
+            answer = mean(get_num(operation))
+            if answer is None:
+                pass
+            else:
+                print('Your answer is {}'.format(answer))
+        elif operation is 7:
+            answer = power(get_num(operation))
+            if answer is None:
+                pass
+            else:
+                print('Your answer is {}'.format(answer))
+        elif operation == 8:
+            answer = cosine(get_num(operation))
+            if answer is None:
+                pass
+            else:
+                print('Your answer is {}'.format(answer))
+        elif operation == 9:
+            answer = get_num(operation)
+            if answer is None:
+                pass
+            else:
+                print('Your answer is', end=' ')
+                for x in range(answer[0], answer[1]+1):
+                    print(x, end=' ')
